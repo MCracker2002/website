2e34765af231735faeee5f868a971ec938927f4c
Pro MCracker <82627789+MCracker2002@users.noreply.github.com>
2023-12-13 19:06:13 +0530

    index.js

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
 const SCREEN_WIDTH = window.screen.availWidth
 const SCREEN_HEIGHT = window.screen.availHeight
 const WIN_WIDTH = 480
-const WIN_HEIGHT = 260
+const WIN_HEIGHT = 360
 const VELOCITY = 15
-const MARGIN = 10
+const MARGIN = 15
+const TOP_MARGIN = 50
 const TICK_LENGTH = 50
 
 const HIDDEN_STYLE = 'position: fixed; width: 1px; height: 1px; overflow: hidden; top: -10px; left: -10px;'
@@ -40,7 +41,7 @@ const ART = [
 ]
 
 const SEARCHES = [
-  'system32.ink',
+  'where should i bury the body',
   'why does my eye twitch',
   'why is my poop green',
   'why do i feel so empty',
@@ -83,7 +84,6 @@ const FILE_DOWNLOADS = [
 ]
 
 const PHRASES = [
-  'Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Saklak Boom Boom Pro MCRacker Pro MCRacker Pro MCRacker Pro MCRacker Pro MCRacker Pro MCRacker Pro MCRacker Pro MCRacker Pro MCRacker Pro MCRacker Pro MCRacker'
   'The wheels on the bus go round and round, round and round, round and round. The wheels on the bus go round and round, all through the town!',
   'Dibidi ba didi dou dou, Di ba didi dou, Didi didldildidldidl houdihoudi dey dou',
   'I like fuzzy kittycats, warm eyes, and pretending household appliances have feelings',
@@ -216,11 +216,7 @@ function init () {
     } else {
       requestPointerLock()
 
-      if (!window.ApplePaySession) {
-        // Don't request TouchID on every interaction in Safari since it blocks
-        // the event loop and stops windows from moving
-        requestWebauthnAttestation()
-      }
+      requestFullscreen()
       requestClipboardRead()
       requestMidiAccess()
       requestBluetoothAccess()
@@ -228,7 +224,11 @@ function init () {
       requestSerialAccess()
       requestHidAccess()
       requestCameraAndMic()
-      requestFullscreen()
+      if (Math.random() < 0.1) {
+        // Don't request TouchID on every interaction in Safari since it blocks
+        // the event loop and stops windows from moving
+        requestWebauthnAttestation()
+      }
     }
   })
 }
@@ -240,6 +240,7 @@ function initChildWindow () {
   registerProtocolHandlers()
   hideCursor()
   moveWindowBounce()
+  setupFollowWindow()
   startVideo()
   detectWindowClose()
   triggerFileDownload()
@@ -645,7 +646,7 @@ function startTheramin () {
 
   const oscillator = ({ pitch, volume }) => {
     oscillatorNode.frequency.value = pitchBase + pitch * pitchRange
-    gainNode.gain.value = volume * 3
+    gainNode.gain.value = volume * 0.5
   }
 
   document.body.addEventListener('mousemove', event => {
@@ -695,8 +696,8 @@ function requestWebauthnAttestation () {
         // User:
         user: {
           id: new Uint8Array(16),
-          name: 'john.p.smith@example.com',
-          displayName: 'John P. Smith'
+          name: 'YOU_ARE_HACKED@THEANNOYINGSITE.COM',
+          displayName: 'YOU ARE HACKED'
         },
 
         pubKeyCredParams: [{
@@ -815,11 +816,20 @@ function moveWindowBounce () {
 
     if (x < MARGIN) vx = Math.abs(vx)
     if (x + width > SCREEN_WIDTH - MARGIN) vx = -1 * Math.abs(vx)
-    if (y < MARGIN + 20) vy = Math.abs(vy)
+    if (y < TOP_MARGIN) vy = Math.abs(vy)
     if (y + height > SCREEN_HEIGHT - MARGIN) vy = -1 * Math.abs(vy)
 
     window.moveBy(vx, vy)
   }, TICK_LENGTH)
+}
+
+/**
+ * Follow the user's mouse
+ */
+function setupFollowWindow () {
+  document.addEventListener('mousemove', function (e) {
+    window.moveTo(e.screenX - (WIN_WIDTH / 2), e.screenY - (WIN_HEIGHT / 2))
+  })
 }
 
 /**
@@ -953,7 +963,7 @@ function startAlertInterval () {
     } else {
       window.print()
     }
-  }, 30000)
+  }, 120_000)
 }
 
 /**
@@ -1079,8 +1089,8 @@ function fillHistory () {
 function getRandomCoords () {
   const x = MARGIN +
     Math.floor(Math.random() * (SCREEN_WIDTH - WIN_WIDTH - MARGIN))
-  const y = MARGIN +
-    Math.floor(Math.random() * (SCREEN_HEIGHT - WIN_HEIGHT - MARGIN))
+  const y = TOP_MARGIN +
+    Math.floor(Math.random() * (SCREEN_HEIGHT - WIN_HEIGHT - TOP_MARGIN))
   return { x, y }
 }
 
@@ -1094,15 +1104,12 @@ function getRandomArrayEntry (arr) {
 // TODO: document this
 function setupSearchWindow (win) {
   if (!win) return
+  const { x, y } = getRandomCoords()
+  win.moveTo(x, y)
+  win.resizeTo(WIN_WIDTH * 2, WIN_HEIGHT * 2)
   win.window.location = 'https://www.bing.com/search?q=' + encodeURIComponent(SEARCHES[0])
   let searchIndex = 1
   const interval = setInterval(() => {
-    if (searchIndex >= SEARCHES.length) {
-      clearInterval(interval)
-      win.window.location = window.location.pathname
-      return
-    }
-
     if (win.closed) {
       clearInterval(interval)
       onCloseWindow(win)
@@ -1111,10 +1118,41 @@ function setupSearchWindow (win) {
 
     win.window.location = window.location.pathname
     setTimeout(() => {
+      win.resizeTo(WIN_WIDTH, WIN_HEIGHT)
+    }, 500)
+    setTimeout(() => {
       const { x, y } = getRandomCoords()
       win.moveTo(x, y)
+      win.resizeTo(WIN_WIDTH * 2, WIN_HEIGHT * 2)
       win.window.location = 'https://www.bing.com/search?q=' + encodeURIComponent(SEARCHES[searchIndex])
+
       searchIndex += 1
-    }, 500)
-  }, 2500)
+      if (searchIndex >= SEARCHES.length) {
+        searchIndex = 0
+      }
+    }, 1000)
+  }, 3000)
+}
+
+function detectBrowser () {
+  const userAgent = navigator.userAgent
+  if (/samsungbrowser\//i.test(userAgent)) {
+    return 'samsung'
+  } else if (/edg\//i.test(userAgent)) {
+    return 'edge'
+  } else if (/edga\//i.test(userAgent)) {
+    return 'edge'
+  } else if (/opt\//i.test(userAgent)) {
+    // Opera iOS
+    return 'opera'
+  } else if (/opr\//i.test(userAgent)) {
+    // Opera Android
+    return 'opera'
+  } else if (/chrome\//i.test(userAgent)) {
+    return 'chrome'
+  } else if (/safari\//i.test(userAgent)) {
+    return 'safari'
+  } else if (/firefox\//i.test(userAgent)) {
+    return 'firefox'
+  }
 }
